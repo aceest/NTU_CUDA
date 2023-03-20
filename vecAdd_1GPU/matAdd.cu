14488df10594345b43449e66bd026e7f4bc242e7
aceest <aceestZ@gmail.com>
2023-03-20 20:16:31 +0800

    commit message

--- vecAdd_1GPU/matAdd.cu
+++ vecAdd_1GPU/matAdd.cu
@@ -1,4 +1,4 @@
-// Vector addition: C = 1/A + 1/B.
+// Matrix addition: C = A + B.
 // compile with the following command:
 //
 // (for GTX1060)
@@ -9,12 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 // Variables
-float* h_A;   // host vectors
+
+float* h_A;   // host matrix
 float* h_B;
 float* h_C;
 float* h_D;
-float* d_A;   // device vectors
+float* d_A;   // device matrix
 float* d_B;
 float* d_C;
 
@@ -22,21 +25,21 @@ float* d_C;
 void RandomInit(float*, int);
 
 // Device code
-__global__ void VecAdd(const float* A, const float* B, float* C, int N)
-{
-
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < N)
-        C[i] = 1.0/A[i] + 1.0/B[i];
-    
+__global__ void matAdd(float* A, float* B, float* C, int N){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < N && j < N) {
+	    int index = i*N + j;
+	    C[index] = A[index] + B[index];
+    //	C[i][j] = A[i][j] + B[i][j];
+    }
     __syncthreads();
-
 }
 
 // Host code
 
-int main( )
-{
+int main( ){
+    
 
     int gid;   // GPU_ID
 
@@ -55,68 +58,62 @@ int main( )
 
     cudaSetDevice(gid);
 
-    printf("Vector Addition: C = 1/A + 1/B\n");
     int mem = 1024*1024*1024;     // Giga    
     int N;
 
     printf("Enter the size of the vectors: ");
     scanf("%d",&N);        
     printf("%d\n",N);        
-    if( 2*N > mem ) {     // each real number (float) takes 4 bytes
-      printf("The size of these 3 vectors cannot be fitted into 6 Gbyte\n");
-      exit(2);
-    }
-    long size = N * sizeof(float);
+    if( N*N > mem ) {     // each real number (float) takes 4 bytes
+	 printf("The size of these 3 vectors cannot be fitted into 6 Gbyte\n");
+	 exit(2);
+	 }
 
+    // Allocate input matrix h_A and h_B in host memory
 
-    // Allocate input vectors h_A and h_B in host memory
-
-    h_A = (float*)malloc(size);
-    h_B = (float*)malloc(size);
-    h_C = (float*)malloc(size);
+    h_A = (float*)malloc(N * N * sizeof(float));
+    h_B = (float*)malloc(N * N * sizeof(float));
+    h_C = (float*)malloc(N * N * sizeof(float));
     
-    // Initialize the input vectors with random numbers
-
-    RandomInit(h_A, N);
-    RandomInit(h_B, N);
+    // Initialize the input matrix with random numbers
+    srand(0);
+    RandomInit(h_A, N*N);
+    RandomInit(h_B, N*N);
 
     // Set the sizes of threads and blocks
 
     int threadsPerBlock;
-loop:
+    
     printf("Enter the number of threads per block: ");
     scanf("%d",&threadsPerBlock);
     printf("%d\n",threadsPerBlock);
-    if( threadsPerBlock > 1024 ) {
-      printf("The number of threads per block must be less than 1024 ! \n");
-      goto loop;
-    }
-    int blocksPerGrid = (N + threadsPerBlock - 1)/threadsPerBlock;
-    printf("The number of blocks is %d\n", blocksPerGrid);
-    if( blocksPerGrid > 2147483647 ) {
-      printf("The number of blocks must be less than 2147483647 ! \n");
-      goto loop;
-    }
-
+    
+    
+   
+   
+    dim3 block(threadsPerBlock, threadsPerBlock);
+    dim3 numBlocks((N + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+    
     // create the timer
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    cudaevent_t start, stop;
+    cudaeventcreate(&start);
+    cudaeventcreate(&stop);
 
     // start the timer
-    cudaEventRecord(start,0);
+    cudaeventrecord(start,0);
 
-    // Allocate vectors in device memory
+    // allocate vectors in device memory
 
-    cudaMalloc((void**)&d_A, size);
-    cudaMalloc((void**)&d_B, size);
-    cudaMalloc((void**)&d_C, size);
+    cudamalloc((void**)&d_A,  N * N * sizeof(float));
+    cudaMalloc((void**)&d_B,  N * N * sizeof(float));
+    cudaMalloc((void**)&d_C,  N * N * sizeof(float));
 
-    // Copy vectors from host memory to device memory
+    // Copy matrix from host memory to device memory
 
-    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
-    
+    cudaMemcpy(d_A, h_A, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, N * N * sizeof(float), cudaMemcpyHostToDevice);
+
+ 
     // stop the timer
     cudaEventRecord(stop,0);
     cudaEventSynchronize(stop);
@@ -128,7 +125,7 @@ loop:
     // start the timer
     cudaEventRecord(start,0);
 
-    VecAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
+    matAdd<<<numBlocks, block>>>(d_A, d_B, d_C, N);
     
     // stop the timer
     cudaEventRecord(stop,0);
@@ -137,7 +134,7 @@ loop:
     float gputime;
     cudaEventElapsedTime( &gputime, start, stop);
     printf("Processing time for GPU: %f (ms) \n",gputime);
-    printf("GPU Gflops: %f\n",3*N/(1000000.0*gputime));
+    printf("GPU Gflops: %f\n",3*N*N/(1000000.0*gputime));
     
     // Copy result from device memory to host memory
     // h_C contains the result in host memory
@@ -145,7 +142,7 @@ loop:
     // start the timer
     cudaEventRecord(start,0);
 
-    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_C, d_C, N * N * sizeof(float), cudaMemcpyDeviceToHost);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
@@ -164,11 +161,13 @@ loop:
 
     // start the timer
     cudaEventRecord(start,0);
-
-    h_D = (float*)malloc(size);       // to compute the reference solution
-    for (int i = 0; i < N; ++i) 
-        h_D[i] = 1.0/h_A[i] + 1.0/h_B[i];
     
+    h_D = (float*)malloc( N * N * sizeof(float));       // to compute the reference solution
+    for (int i = 0; i < N; ++i){ 
+	    for (int j = 0; j <N; ++j){
+        	h_D[i][j] = h_A[i][j] + h_B[i][j];
+	    }
+    }
     // stop the timer
     cudaEventRecord(stop,0);
     cudaEventSynchronize(stop);
@@ -176,7 +175,7 @@ loop:
     float cputime;
     cudaEventElapsedTime( &cputime, start, stop);
     printf("Processing time for CPU: %f (ms) \n",cputime);
-    printf("CPU Gflops: %f\n",3*N/(1000000.0*cputime));
+    printf("CPU Gflops: %f\n",3*N*N/(1000000.0*cputime));
     printf("Speed up of GPU = %f\n", cputime/(gputime_tot));
 
     // destroy the timer
@@ -189,8 +188,10 @@ loop:
     double sum=0; 
     double diff;
     for (int i = 0; i < N; ++i) {
-      diff = abs(h_D[i] - h_C[i]);
-      sum += diff*diff; 
+	    for ( int j = 0; j < N; ++j){
+      		diff = abs(h_D[i][j] - h_C[i][j]);
+      		sum += diff*diff; 
+	    }
 //      if(diff > 1.0e-15) { 
 //        printf("i=%d, h_D=%15.10e, h_C=%15.10e \n", i, h_D[i], h_C[i]);
 //      }
